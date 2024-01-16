cd51d9a6fa60dec5865e849379cadc80154310f9
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 12:06:25 +0900

    fix: path

--- public/views/books/create.php
+++ public/views/books/create.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../src/controllers/BookController.php';
+require_once __DIR__ . '/../../../src/controllers/BookController.php';
 
 $name = $_POST['name'];
 $description = $_POST['description'];

c1e7d685cacdc1411d544dfcf847eb952420db59
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 09:48:38 +0900

    add: PdoManager

---
+++ src/PdoManager.php
@@ -0,0 +1,22 @@
+<?php
+class PdoManager {
+  private $pdo;
+
+  public function __construct() {}
+
+  public static function getPdo() {
+    if(self::$pdo == null) {
+      $config = parse_ini_file('../config.ini');
+      $host = $config['host'];
+      $dbname = $config['dbname'];
+      $user = $config['user'];
+      $password = $config['password'];
+
+      self::$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+      self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    return self::$pdo;
+  }
+}
+?>

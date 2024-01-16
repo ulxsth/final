ee15ba13ef72518b20745d92509868943d983293
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 12:38:38 +0900

    fix: echo忘れ

--- public/views/books/show.php
+++ public/views/books/show.php
@@ -9,12 +9,12 @@ $book = BookController::find($_GET['id']);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php $book->getName(); ?></title>
+  <title><?php echo $book->getName(); ?></title>
 </head>
 <body>
-  <h1><?php $book->getName(); ?></h1>
-  <p><?php $book->getDescription(); ?></p>
-  <a href="/books/<?php $book->getId(); ?>/edit">編集</a>
-  <a href="/books/<?php $book->getId(); ?>/delete">削除</a>
+  <h1><?php echo $book->getName(); ?></h1>
+  <p><?php echo $book->getDescription(); ?></p>
+  <a href="/books/<?php echo $book->getId(); ?>/edit">編集</a>
+  <a href="/books/<?php echo $book->getId(); ?>/delete">削除</a>
 </body>
 </html>

d8867e498a5c5eb77b10a68ff842534ab03a7a45
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 12:26:28 +0900

    fix: BookDtoのパラメータをprivateに

--- src/dtos/BookDto.php
+++ src/dtos/BookDto.php
@@ -1,15 +1,31 @@
 <?php
 class BookDto {
-  public $id;
-  public $name;
-  public $description;
-  public $isRead;
+  private $id;
+  private $name;
+  private $description;
+  private $isRead;
 
   public function __construct($id, $name, $description, $isRead) {
     $this->id = $id;
     $this->name = $name;
     $this->description = $description;
     $this->isRead = $isRead;
+  }
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getDescription() {
+    return $this->description;
+  }
+
+  public function getIsRead() {
+    return $this->isRead;
   }
 }
 ?>

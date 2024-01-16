499947925bee6dcda2dbafadfe38f7d0e00cd295
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 12:20:48 +0900

    fix: path

--- src/PdoManager.php
+++ src/PdoManager.php
@@ -6,11 +6,11 @@ class PdoManager {
 
   public static function getPdo() {
     if(self::$pdo == null) {
-      $config = parse_ini_file('../env.ini');
-      $host = $config['host'];
-      $dbname = $config['dbname'];
-      $user = $config['user'];
-      $password = $config['password'];
+      $config = parse_ini_file(__DIR__ . '/../env.ini');
+      $host = $config['HOST'];
+      $dbname = $config['DBNAME'];
+      $user = $config['USER'];
+      $password = $config['PASSWORD'];
 
       self::$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
       self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

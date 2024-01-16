ac4b83b20514e6ce1590b87c4a6e9deb4a22284b
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-16 11:18:51 +0900

    add: books/create

---
+++ public/views/books/create.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../../src/controllers/BookController.php';
+
+$name = $_POST['name'];
+$description = $_POST['description'];
+
+$id = BookController::create($name, $description);
+header('Location: /books/' . $id);
+?>

7337dc1de58a08042c4ec5b0bd3df0304169249f
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-17 09:42:31 +0900

    refactor: if文の最適化

--- public/views/books/index.php
+++ public/views/books/index.php
@@ -6,11 +6,13 @@ $books = BookController::all();
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>一覧</title>
 </head>
+
 <body>
   <h1>一覧</h1>
   <a href="/views/books/create.php">新規作成</a>
@@ -30,15 +32,16 @@ $books = BookController::all();
           <td><?php echo $book->getDescription() ?></td>
           <td><?php echo $book->getIsRead() ? '既読' : '未読' ?></td>
           <td>
-            <?php if ($book->getIsRead()) { ?>
-              <a href="/final/books/uncheck/<?php echo $book->getId() ?>">未読にする</a>
-            <?php } else { ?>
-              <a href="/final/books/check/<?php echo $book->getId() ?>">既読にする</a>
-            <?php } ?>
+            <?php if ($book->getIsRead()) : ?>
+              <a href="/final/books/<?php echo $book->getId() ?>/uncheck">未読にする</a>
+            <?php else: ?>
+              <a href="/final/books/<?php echo $book->getId() ?>/check">既読にする</a>
+            <?php endif ?>
           </td>
         </tr>
       <?php } ?>
     </tbody>
   </table>
 </body>
+
 </html>
